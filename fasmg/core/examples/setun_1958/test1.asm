a1fa8219d63a89fccdc9b904ec890374daf0f282
Vladimir V <vasilyevvi@baltech.ru>
2022-06-01 16:30:04 +0300

    fasmg

---
+++ fasmg/core/examples/setun_1958/test1.asm
@@ -0,0 +1,12 @@
+
+; This simple example reads data from external device through P2
+; and outputs it inverted through P1.
+
+include 'hex.inc'
+include 'setun_1958.inc'
+
+
+ORG 0001
+	jmp	0001
+
+
---
+++ fasmg/core/test.asm
@@ -0,0 +1,6 @@
+;include 'hex.inc'
+include 'setun_1958.inc'
+
+;ORG 1
+;   ld F,-3
+    ter_instr Halt,0
